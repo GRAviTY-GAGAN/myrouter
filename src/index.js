d6ee264f3975ee0ab34c262f7286adf7bd565b76
GRAviTY-GAGAN <68629598+GRAviTY-GAGAN@users.noreply.github.com>
2022-06-29 02:36:50 -0400

    Created Routes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { Routes, Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Visit from './Visit';
+import User from './User';
+import Notfound from './Notfound';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const routing = (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/User'>User</Link></li>
+        <li><Link to='/Visit'>Visit</Link></li>
+      </ul>
+    </div>
+    {/* Switch is replaced by Routes in V6 */}
+    {/* To Redirect to a "default" route if no other routes match: we use path='*'  */}
+    <Routes> 
+    <Route exact path='/' element={<App />} />
+    <Route path='/User' element={<User />} />
+    <Route path='/Visit' element={<Visit />} />
+    <Route path='*'  element={<Notfound />} />
+    </Routes>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
